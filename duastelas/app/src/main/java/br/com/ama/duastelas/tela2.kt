70369eeb01369021b88a652e107c3e9dcdd79f42
aMarceloAlmeida <amalemida@gmail.com>
2023-03-15 20:50:14 -0300

    Acionando a terceira tela e passando Valores para a segunda tela

--- duastelas/app/src/main/java/br/com/ama/duastelas/tela2.kt
+++ duastelas/app/src/main/java/br/com/ama/duastelas/tela2.kt
@@ -2,10 +2,34 @@ package br.com.ama.duastelas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+import android.widget.TextView
 
 class tela2 : AppCompatActivity() {
+
+    //lateinit var btnVoltar : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela2)
+        //recebendo valores da tela que chamou
+
+        val string: String? = intent.getStringExtra("Cha")
+
+        val texto = findViewById(R.id.textView2) as TextView
+        texto.setText(string)
+
+        val btnVoltar = findViewById(R.id.btnVoltar) as Button
+        btnVoltar.setOnClickListener {
+            this.finish()
+        }
+
+        val btnNext = findViewById(R.id.btnGoTo3) as Button
+        btnNext.setOnClickListener {
+            val intent = Intent(this, tela3::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+        }
     }
 }
---
+++ duastelas/app/src/main/java/br/com/ama/duastelas/tela3.kt
@@ -0,0 +1,27 @@
+package br.com.ama.duastelas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class tela3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela3)
+
+        Log.d("Aula","Tela3")
+
+        val btnVoltar = findViewById(R.id.btnVoltar3) as Button
+        btnVoltar.setOnClickListener {
+            this.finish()
+        }
+        val btnVini = findViewById(R.id.btnBack1) as Button
+        btnVini.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+        }
+    }
+}
